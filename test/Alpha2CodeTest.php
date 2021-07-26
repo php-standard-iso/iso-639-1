036a5cc3b1c910e46c468c62e5460f89d2f6b51d
Gary Gitton <gary.gitton@gmail.com>
2021-07-26 04:03:00 +0300

    add test

---
+++ test/Alpha2CodeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iso639UnitTest\Part1;
+
+use Iso639\Part1\Alpha2Code;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+use function ctype_lower;
+use function ctype_upper;
+use function str_replace;
+
+class Alpha2CodeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function constantValuesAreLowercase()
+    {
+        $reflectionClass = new ReflectionClass(Alpha2Code::class);
+        $values          = $reflectionClass->getConstants();
+
+        foreach ($values as $value) {
+            $this->assertTrue(ctype_lower($value));
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function constantKeysAreUpperCase()
+    {
+        $reflectionClass = new ReflectionClass(Alpha2Code::class);
+        $values          = $reflectionClass->getConstants();
+
+        foreach ($values as $key => $value) {
+            $keyWithoutUnderscore = str_replace('_', '', $key);
+
+            $this->assertTrue(ctype_upper($keyWithoutUnderscore));
+        }
+    }
+}
